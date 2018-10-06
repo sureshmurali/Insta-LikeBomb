8092a9245dd19039f7cde4ea0aab7a718d6312fb
Suresh Murali <m.suresh.fx@gmail.com>
2018-10-05 22:17:54 -0700

    ✨ Feat: loads instagram login page

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      '--window-size=1920,1080',
+    ],
+  });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
-
+  page.setViewport({ height: 1080, width: 1920 });
+  await page.goto('https://instagram.com/accounts/login');
   await browser.close();
 })();
