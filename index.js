ed6aaa497141b775cb2305a0ac37e2c5f1da0305
Suresh Murali <m.suresh.fx@gmail.com>
2018-10-05 23:23:47 -0700

    ✨ Feat: login and go to victim's page

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const puppeteer = require('puppeteer');
 require('dotenv').config();
 
-const { INSTA_USERNAME, INSTA_PASSWORD } = process.env;
+const {
+  INSTA_USERNAME,
+  INSTA_PASSWORD,
+  INSTA_LIKEBOMB_VICTIM_ID,
+} = process.env;
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
-    args: [
-      '--window-size=1920,1080',
-    ],
+    args: ['--window-size=1280,1000'],
     slowMo: 10,
   });
   const page = await browser.newPage();
-  page.setViewport({ height: 1080, width: 1920 });
+  page.setViewport({ width: 1280, height: 720 });
   await page.goto('https://instagram.com/accounts/login');
   await page.waitFor(() => document.querySelectorAll('input').length);
   await page.type('[name=username]', INSTA_USERNAME);
   await page.type('[name=password]', INSTA_PASSWORD);
+  await page.evaluate(() => {
+    document.querySelector('.oF4XW').click();
+  });
+  await page.waitForNavigation();
+  await page.goto(`https://www.instagram.com/${INSTA_LIKEBOMB_VICTIM_ID}/`);
+  await page.waitFor(2000);
   await browser.close();
 })();
