6084faf0c9dfe6f45b4e18031bb9f356d5979f77
Suresh Murali <m.suresh.fx@gmail.com>
2018-10-05 22:31:33 -0700

    ✨ Feat: input login credentials from dotenv file

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const puppeteer = require('puppeteer');
+require('dotenv').config();
+
+const { INSTA_USERNAME, INSTA_PASSWORD } = process.env;
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -6,9 +9,13 @@ const puppeteer = require('puppeteer');
     args: [
       '--window-size=1920,1080',
     ],
+    slowMo: 10,
   });
   const page = await browser.newPage();
   page.setViewport({ height: 1080, width: 1920 });
   await page.goto('https://instagram.com/accounts/login');
+  await page.waitFor(() => document.querySelectorAll('input').length);
+  await page.type('[name=username]', INSTA_USERNAME);
+  await page.type('[name=password]', INSTA_PASSWORD);
   await browser.close();
 })();
